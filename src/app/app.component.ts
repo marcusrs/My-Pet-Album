e1d5b1b76cb6182e3c1b0c13391c5fe7d3802817
marcus shipman <marcus shipman>
2017-08-17 12:52:31 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // Define a users property to hold  user data
+  users: Array<any>;
+
+  // Create an instance of the DataService through dependency injection
+    constructor(private _dataService: DataService) {
+  // Access the Data Service's getUsers() method
+      this._dataService.getUsers()
+          .subscribe(res => this.users = res);
+    }
 }
