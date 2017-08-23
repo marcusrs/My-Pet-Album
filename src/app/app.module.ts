576193854e6c34d35aac8d260dbc512afb88c1c1
marcusrs <marcusshipman@yahoo.com>
2017-08-23 23:54:17 +0000

    added angular material and angular animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule} from '@angular/material';
+
 // Import the Http Module and our Data Service
 import { HttpModule } from '@angular/http';
 import { DataService } from './data.service';
@@ -12,9 +15,17 @@ import { DataService } from './data.service';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCardModule,
+    MdMenuModule,
+    MdIconModule,
+    MdToolbarModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export class PizzaPartyAppModule { }
